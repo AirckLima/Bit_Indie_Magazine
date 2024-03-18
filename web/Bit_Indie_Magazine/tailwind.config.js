c93d69f9b3b223250005556fdca48379db9d998a
AirckLima <aircklima@gmail.com>
2024-03-18 10:37:52 -0300

    style: finished base style

--- web/Bit_Indie_Magazine/tailwind.config.js
+++ web/Bit_Indie_Magazine/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
+            fontSize: {
+                "uxs": "0.56rem"
+            },
             fontFamily: {
                 "honk": ['"Honk",  system-ui' , {fontVariationSettings: '"MORF" 45, "SHLN" 0' }],
                 "kode": ['"Kode Mono",  monospace'],
