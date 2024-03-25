1d3ce89562f352536a8d765cd84ac9227831134c
Airck Lima <aircklima@gmail.com>
2024-03-25 19:07:41 -0300

    style: text to editions page on mobile link list

--- bit_indie_magazine/src/components/site/header/LinkList.tsx
+++ bit_indie_magazine/src/components/site/header/LinkList.tsx
@@ -11,11 +11,10 @@ const LinkList = ({ classStyle, onLinkClick }: LinkListProps) => {
 
     return (
         <ul className={classStyle}>
-            <li><Link href={"/editions"} className={` ${path === "/editions" ? "pointer-events-none text-slate-400 " : ""} `} onClick={onLinkClick}>Link 1</Link></li>
+            <li><Link href={"/editions"} className={` ${path === "/editions" ? "pointer-events-none text-slate-400 " : ""} `} onClick={onLinkClick}>Editions</Link></li>
             <li><Link href={"/"} className={` ${path === "/" ? "pointer-events-none text-slate-400 " : ""} `} onClick={onLinkClick}>Link 2</Link></li>
             <li><Link href={"/"} className={` ${path === "/" ? "pointer-events-none text-slate-400 " : ""} `} onClick={onLinkClick}>Link 3</Link></li>
             <li><Link href={"/"} className={` ${path === "/" ? "pointer-events-none text-slate-400 " : ""} `} onClick={onLinkClick}>Link 4</Link></li>
-
         </ul>
     )
 }
