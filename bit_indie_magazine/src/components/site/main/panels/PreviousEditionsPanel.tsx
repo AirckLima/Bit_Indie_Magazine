a95696a1b9eadadae7365f159985d89d2a9cc104
Airck Lima <aircklima@gmail.com>
2024-03-29 19:30:46 -0300

    build: added Prisma ORM

--- bit_indie_magazine/src/components/site/main/panels/PreviousEditionsPanel.tsx
+++ bit_indie_magazine/src/components/site/main/panels/PreviousEditionsPanel.tsx
@@ -1,6 +1,9 @@
 import Panel from "./Panel";
 
 const PreviousEditionsPanel = () => {
+    const previousEdition = [1, 2, 3, 4, 5, 6]
+    let i = 0;
+
     return (
         <Panel panelId="previous-editions-panel" backgroundImage="" backgroundColor="bg-indigo-950">
             <h2 className=" font-lexend text-2xl">Previous Editions</h2>
@@ -10,21 +13,16 @@ const PreviousEditionsPanel = () => {
                 <div className="absolute pointer-events-none right-0 top-0 left-0 bottom-0  bg-gradient-to-r from-transparent via-transparent via-[95%] to-indigo-950 to-[100%] opacity-90"></div>
                 <div className="overflow-x-scroll will-change-scroll snap-x">
                     <div id="previous-editions-showcase" className=" flex flex-row justify-between w-[160%] py-2  pr-5 pl-2 ">
-                        <div id="edition-index-1" className="w-16 h-24 shrink-0 bg-orange-400 rounded-sm snap-start scroll-mx-3">
-                            <img src="" />
-                        </div>
-                        <div id="edition-index-2" className="w-16 h-24 shrink-0 bg-orange-400 rounded-sm snap-start scroll-mx-3">
-                            <img src="" />
-                        </div>
-                        <div id="edition-index-3" className="w-16 h-24 shrink-0 bg-orange-400 rounded-sm snap-start scroll-mx-3">
-                            <img src="" />
-                        </div>
-                        <div id="edition-index-4" className="w-16 h-24 shrink-0 bg-orange-400 rounded-sm snap-start scroll-mx-3">
-                            <img src="" />
-                        </div>
-                        <div id="edition-index-5" className="w-16 h-24 shrink-0 bg-orange-400 rounded-sm snap-start scroll-mx-3">
-                            <img src="" />
-                        </div>
+
+                        {
+                            previousEdition.map((edition) =>
+                            (
+                                <li key={++i} id={`edition-index-${edition}`} className="group relative w-16 h-24 shrink-0 bg-orange-400 rounded-sm snap-start scroll-mx-3 list-none">
+                                    <a href={`/editions/${edition}`} className="block size-full">
+                                    </a>
+                                </li>
+                            ))
+                        }
 
                         <a href="/editions" className="self-center" >
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 self-center mr-3">
